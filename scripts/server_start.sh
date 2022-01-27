6eb226fb9c12bf638f1b17156c518f2c0bca3288
Marcelo Schambeck <mschambeck@gmail.com>
2022-01-27 02:05:47 -0300

    AWS CodeDeploy

--- scripts/server_start.sh
+++ scripts/server_start.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 cd /home/ec2-user/app/target
-sudo java -jar spring-webmvc-0.0.1-SNAPSHOT.jar &
+sudo java -jar spring-webmvc-0.0.1-SNAPSHOT.jar > /dev/null 2> /dev/null < /dev/null &
